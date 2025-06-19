e0b5442870c21ed38cca1af70d41dda5f726a7fa
Weston Vincze <westonvincze@gmail.com>
2025-06-18 20:27:12 -0400

    💄 Improve responsiveness

--- src/react-components/App.tsx
+++ src/react-components/App.tsx
@@ -3,10 +3,11 @@ import { useActions, useQuery } from "koota/react";
 
 import { SceneContainer } from "./SceneContainer";
 import { exampleActions } from "../ecs";
-import { IsAnt, Position } from "../ecs/traits";
+import { IsAnt, IsFood, Position } from "../ecs/traits";
 
 export default function App() {
-  const entities = useQuery(IsAnt, Position);
+  const ants = useQuery(IsAnt, Position);
+  const food = useQuery(IsFood, Position);
   const { spawnAnt, removeAnt, spawnFood, removeFood } = useActions(exampleActions);
 
   return (
@@ -15,28 +16,35 @@ export default function App() {
         <SceneContainer/>
       </Canvas>
 
-      <div className="absolute bottom-5 left-5 flex gap-5">
-        <div onClick={spawnAnt} className={"btn btn-green"}>
-          Spawn Ant!
+      <div className="absolute bottom-5 left-5 flex flex-col gap-2">
+        
+        <div className="flex gap-8">
+          <div className="text-xl">
+            Ants: <b>{ants.length}</b>
+          </div>
+          <div className="text-xl">
+            Food: <b>{food.length}</b>
+          </div>
         </div>
 
-        <div onClick={removeAnt} className={"btn btn-red"}>
-          Remove Ant!
-        </div>
+        <div className="flex gap-5 max-w-full flex-wrap">
+          <div onClick={spawnAnt} className={"btn btn-green"}>
+            + Ant
+          </div>
 
-        <div onClick={spawnFood} className={"btn btn-purple"}>
-          Spawn Food!
-        </div>
+          <div onClick={removeAnt} className={"btn btn-red"}>
+            - Ant
+          </div>
 
-        <div onClick={removeFood} className={"btn btn-red"}>
-          Remove Food!
+          <div onClick={spawnFood} className={"btn btn-purple"}>
+            + Food
+          </div>
+
+          <div onClick={removeFood} className={"btn btn-red"}>
+            - Food
+          </div>
         </div>
       </div>
-
-      <div className={"absolute bottom-20 left-5"} style={{fontSize: "2.5rem"}}>
-        Number of Ants: {entities.length}
-      </div>
-
     </div>
   )
 }
