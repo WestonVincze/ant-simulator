22f0bccde4cc5afaeb1b2e2385ced8c6a451a2f7
Weston Vincze <westonvincze@gmail.com>
2026-06-25 12:36:50 -0400

    ⚡️ Optimize pheromones - change instancedMesh to points and use a custom shader

--- src/ecs/traits/refs.ts
+++ src/ecs/traits/refs.ts
@@ -1,11 +1,11 @@
 import { trait } from "koota";
-import { AnimationAction, AnimationMixer, InstancedMesh, Mesh } from "three";
+import { AnimationAction, AnimationMixer, Mesh, Points } from "three";
 
 // we only use this mesh as an init value for types, we'll pass the actual mesh when adding this trait
 export const MeshRef = trait({ref: new Mesh});
 
-export const FoodPheromoneMeshRef = trait<{ ref: InstancedMesh | null }>({ ref: null });
-export const HomePheromoneMeshRef = trait<{ ref: InstancedMesh | null }>({ ref: null });
+export const FoodPheromoneMeshRef = trait<{ ref: Points | null }>({ ref: null });
+export const HomePheromoneMeshRef = trait<{ ref: Points | null }>({ ref: null });
 
 export const AnimationRef = trait<{ mixer: AnimationMixer | null, action: AnimationAction | null }>({
   mixer: null,
