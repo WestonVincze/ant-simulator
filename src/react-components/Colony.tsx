fe9940ecbd5cb767db1a784e08c4cde9ecdee051
Weston Vincze <westonvincze@gmail.com>
2026-06-25 12:18:26 -0400

    💄 Improve look of anthill - add entrance hole

--- src/react-components/Colony.tsx
+++ src/react-components/Colony.tsx
@@ -1,24 +1,25 @@
-import { useEffect, useRef } from "react";
-import { Mesh } from "three";
-import { IsColony, MeshRef, Position } from "../ecs/traits";
 import { useQueryFirst } from "koota/react";
+import { IsColony, Position } from "../ecs/traits";
 
 export function Colony () {
   const colony = useQueryFirst(IsColony, Position);
-  const meshRef = useRef<Mesh>(null!);
 
-  useEffect(() => {
-    colony?.add(MeshRef({ ref: meshRef.current }))
+  if (!colony) return null;
 
-    return () => {
-      colony?.remove(MeshRef);
-    };
-  }, [colony]);
+  const pos = colony.get(Position)!;
 
   return (
-    <mesh ref={meshRef} castShadow receiveShadow>
-      <coneGeometry args={[5, 5, 20]} />
-      <meshStandardMaterial color={"tan"} />
-    </mesh>
-  )
+    <group position={[pos.x, 0, pos.z]}>
+      {/* Main mound */}
+      <mesh castShadow receiveShadow position={[0, 1.5, 0]}>
+        <cylinderGeometry args={[0.8, 5, 3, 20]} />
+        <meshStandardMaterial color="#C8A481" roughness={0.95} metalness={0.3} />
+      </mesh>
+      {/* Entrance hole */}
+      <mesh position={[0, 3.01, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <circleGeometry args={[0.6, 24]} />
+        <meshStandardMaterial color="#1a1a1a" roughness={1} metalness={0} />
+      </mesh>
+    </group>
+  );
 }
